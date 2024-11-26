d0fd99b721c33ed68097555c5683700166d1c61f
arthur Windows <rybalkin.arthur@gmail.com>
2024-11-26 15:21:25 +0100

    added exampe responde

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,56 @@ class _MainAppState extends State<MainApp> {
   Future<WeatherData> getWeatherData(String city) async {
     // https://api.weatherapi.com/v1/current.json
 
+    ///Exampe https Respond
+    /*
+{
+  "location": {
+    "name": "Frankfurt",
+    "region": "Hessen",
+    "country": "Germany",
+    "lat": 50.1167,
+    "lon": 8.6833,
+    "tz_id": "Europe/Berlin",
+    "localtime_epoch": 1732623019,
+    "localtime": "2024-11-26 13:10"
+  },
+  "current": {
+    "last_updated_epoch": 1732622400,
+    "last_updated": "2024-11-26 13:00",
+    "temp_c": 11.2,
+    "temp_f": 52.2,
+    "is_day": 1,
+    "condition": {
+      "text": "Overcast",
+      "icon": "//cdn.weatherapi.com/weather/64x64/day/122.png",
+      "code": 1009
+    },
+    "wind_mph": 10.5,
+    "wind_kph": 16.9,
+    "wind_degree": 232,
+    "wind_dir": "SW",
+    "pressure_mb": 1020.0,
+    "pressure_in": 30.12,
+    "precip_mm": 0.08,
+    "precip_in": 0.0,
+    "humidity": 82,
+    "cloud": 75,
+    "feelslike_c": 9.2,
+    "feelslike_f": 48.5,
+    "windchill_c": 8.5,
+    "windchill_f": 47.3,
+    "heatindex_c": 10.6,
+    "heatindex_f": 51.1,
+    "dewpoint_c": 5.6,
+    "dewpoint_f": 42.0,
+    "vis_km": 10.0,
+    "vis_miles": 6.0,
+    "uv": 0.4,
+    "gust_mph": 13.3,
+    "gust_kph": 21.5
+  }
+    */
+
     final Map<String, dynamic> queryParameters = {
       'q': city,
       'key': api_weather
